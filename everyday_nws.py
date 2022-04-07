f3d6bd68677778ce072be09a4d20d34f44ab705a
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-04-07 15:01:59 +0800

    Update everyday_nws.py

--- everyday_nws.py
+++ everyday_nws.py
@@ -35,7 +35,7 @@ def img():
     #日期位置
     draw.text((900, 55), todayYear(), font=font, fill=(255, 255, 0))
     #新闻位置
-    print(news[1])
+    print(news()[1])
     if news()[1] == None:
         draw.text((100,20), news()[0], font=font_small, fill=(255, 255, 255))
     else:
