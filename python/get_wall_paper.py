9272213a5b82941455dfbc3ddde324af7d2fbb2d
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-04-11 17:42:07 +0800

    Update get_wall_paper.py

--- python/get_wall_paper.py
+++ python/get_wall_paper.py
@@ -13,16 +13,16 @@ def main():
 		if page not in pages:
 			pages.append(page)
 	for i in pages:#拼接获取url地址
-		url = "https://wallhaven.cc/search?categories=110&purity=100&atleast=1920x1080&sorting=date_added&order=desc&page={0}".format(i)#1920x1080是图片比率
+		url1 = "https://wallhaven.cc/search?categories=110&purity=100&atleast=1920x1080&sorting=date_added&order=desc&page={0}".format(i)#1920x1080是图片比率
 		print("正在下载第{0}页所有图片".format(i))
-		get_wall_paper(url)
+		get_wall_paper(url1)
 
 def get_wall_paper(url):
 	response = requests.get(url)
 	selector = parsel.Selector(response.text)
-	url = selector.css('.preview::attr(href)').getall()#获取所有图片源地址
+	url2 = selector.css('.preview::attr(href)').getall()#获取所有图片源地址
 	#print("当前页面共有{0}张图片".format(len(url)))
-	for i in tqdm(url):
+	for i in tqdm(url2):
 		re = requests.get(i)
 		selector2 = parsel.Selector(re.text)
 		url2 = selector2.css('#wallpaper::attr(src)').getall()
