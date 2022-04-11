7d7f914fb5122bf93415012a55cddcbce6e16bd4
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-04-11 18:31:25 +0800

    Update get_wall_paper.py

--- python/get_wall_paper.py
+++ python/get_wall_paper.py
@@ -1,35 +1,37 @@
 import requests
 import parsel
 import os
-import time
 import random
+import time
+from tqdm import tqdm
 
 
 def main():
 	pages = []
-	while (len(pages)<3): #随机获取10个不相等的
+	while (len(pages)<5): #随机获取10个不相等的
 		page = random.randint(1,12380)
 		if page not in pages:
 			pages.append(page)
 	for i in pages:#拼接获取url地址
-		url1 = "https://wallhaven.cc/search?categories=110&purity=100&atleast=1920x1080&sorting=date_added&order=desc&page={0}".format(i)#1920x1080是图片比率
+		url = "https://wallhaven.cc/search?categories=110&purity=100&atleast=1920x1080&sorting=date_added&order=desc&page={0}".format(i)#1920x1080是图片比率
 		print("正在下载第{0}页所有图片".format(i))
-		get_wall_paper(url1)
+		get_wall_paper(url)
 
 def get_wall_paper(url):
 	response = requests.get(url,headers = header)
 	selector = parsel.Selector(response.text)
-	url2 = selector.css('.preview::attr(href)').getall()#获取所有图片源地址
+	url = selector.css('.preview::attr(href)').getall()#获取所有图片源地址
+	print(url)
 	print("当前页面共有{0}张图片".format(len(url)))
-	for i in url2:
-		re = requests.get(i,headers = header)
+	for i in tqdm(url):
+		re = requests.get(i,headers=header)
 		selector2 = parsel.Selector(re.text)
-		url3 = selector2.css('#wallpaper::attr(src)').getall()
+		url2 = selector2.css('#wallpaper::attr(src)').getall()
 		title = selector2.css('title::text').get()
 		title = title.split(',')
-		for u in url3:
-			print(u)
+		for u in url2:
 			r = requests.get(u)
+			print(r)
 			with open(r"./1920x1080/"+title[0]+".jpg","wb") as f:
 				f.write(r.content)
 				f.close()
