682ee4ca8c9bd0fa5af69c12966f0058eb629b63
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-04-28 17:59:18 +0800

    Update get_wall_paper.py

--- python/get_wall_paper.py
+++ python/get_wall_paper.py
@@ -1,45 +1,48 @@
+from urllib import request, parse
 import requests
-import parsel
-import os
-import random
-import time
-from tqdm import tqdm
+
+class wzry(object):
+    def __init__(self):
+        self.page = 0  # 抓取的起始页
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64)'}  # 伪装成浏览器
+
+    def get_page(self):
+        try:
+            while self.page <= 1:   # 设置抓取的结束页
+                url = 'https://apps.game.qq.com/cgi-bin/ams/module/ishow/V1.0/query/workList_inc.cgi?'\
+                      'activityId=2735&sVerifyCode=ABCD&sDataType=JSON&iListNum=20&totalpage=0&page='+str(self.page) + '&i' \
+                      'Order=0&iSortNumClose=1&iAMSAc'\
+                      'tivityId=51991&_everyRead=true&iTypeId=2&iFlowId=267733&'\
+                      'iActId=2735&iModuleId=2735&_=1554873059538'
+                self.page = self.page + 1
+                req = request.Request(url, headers=self.headers)
+                response = request.urlopen(req)
+                #print(response)
+                data = response.read()
+                print(data)
+                list_data = eval(data)['List']
+                print(list_data)
+                for ls in list_data:
+                    # 抓取图片url并替换特殊符号
+                    sProdImgNo_6 = ls['sProdImgNo_6'].replace('%3A', ':').replace(
+                        '%2F', '/').replace('%2E', '.').replace('%5F', '_').replace('%2D', '-').replace('200', '0')
+                    sProdName = ls['sProdName']
+                    img_name = parse.unquote(sProdName)  # 解码字符串
+                    img = requests.get(sProdImgNo_6, verify=False)  # 抓取图片
+                    print(f'正在抓取 {img_name} 高清皮肤......')
+                    # 写入文件
+                    with open(f'./1920x1080/{img_name}.jpg', 'wb') as f:
+                        f.write(img.content)
+        except request.URLError as e:
+            if hasattr(e, 'reason'):
+                print(f'抓取失败，失败原因：{e.reason}')
 
 
-def main():
-	pages = []
-	while (len(pages)<2): #随机获取2个不相等的
-		page = random.randint(1,12380)
-		if page not in pages:
-			pages.append(page)
-	for i in pages:#拼接获取url地址
-		url = "https://wallhaven.cc/search?categories=110&purity=100&atleast=1920x1080&sorting=date_added&order=desc&page={0}".format(i)#1920x1080是图片比率
-		print("正在下载第{0}页所有图片".format(i))
-		get_wall_paper(url)
+class main():
+    wzry().get_page()
 
-def get_wall_paper(url):
-	response = requests.get(url,headers = header)
-	selector = parsel.Selector(response.text)
-	url = selector.css('.preview::attr(href)').getall()#获取所有图片源地址
-	print(url)
-	print("当前页面共有{0}张图片".format(len(url)))
-	for i in tqdm(url):
-		re = requests.get(i,headers=header)
-		selector2 = parsel.Selector(re.text)
-		url2 = selector2.css('#wallpaper::attr(src)').getall()
-		title = selector2.css('title::text').get()
-		title = title.split(',')
-		for u in url2:
-			r = requests.get(u)
-			print(r)
-			with open(r"./1920x1080/"+title[0]+".jpg","wb") as f:
-				f.write(r.content)
-				f.close()
-				time.sleep(1)
-			#print("下载完成请注意查看！！")
+
 if __name__ == '__main__':
-	header = {
-		'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
-	}
-	main()
-	print("恭喜你！下载已经全部完成了！！")
+    main()
+    print('抓取完成。。。')
