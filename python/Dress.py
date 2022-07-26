bc9d8b56393218592800243d39f908c85271fa0a
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-07-26 11:07:12 +0800

    Update Dress.py

--- python/Dress.py
+++ python/Dress.py
@@ -4,7 +4,7 @@ import parsel
 import os
 import json
 
-url = "http://www.weather.com.cn/weather1d/101120111.shtml"
+url = "http://www.weather.com.cn/weather1d/101120911.shtml"
 header = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
