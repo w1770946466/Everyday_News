732a513a337b34131e5e872368452e9d748faacf
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-09-29 22:21:06 +0800

    Update AntiAD-main.js

--- black-AD/55dy/AntiAD-main.js
+++ black-AD/55dy/AntiAD-main.js
@@ -1,5 +1,4 @@
 let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/w1770946466/Everyday_News/main/black-AD/55dy/AntiAD-555dy.css" type="text/css" /><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/w1770946466/Everyday_News/main/black-AD/55dy/AntiAD-555dy.js"></script>'
-let body = $response.body
+let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/w1770946466/Everyday_News/main/black-AD/55dy/AntiAD-555dy.css" type="text/css" />'
     .replace(ele, eleReplace)
 $done({ body });
