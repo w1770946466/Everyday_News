df592c2269e5526382a28de1214085f87487a219
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-09-29 21:38:52 +0800

    Update Anti-ad.js

--- Anti-ad.js
+++ Anti-ad.js
@@ -1,7 +1,7 @@
 let rHead = '<head>'
 let newStyle = '<head><link rel=\"stylesheet\" href=\"https://limbopro.com/CSS/baidu.css\" type=\"text/css\">'
 var rBody = '</body>'
-var newJavaScript = '<script type="text/javascript" async="async" src="//limbopro.com/Adguard/baidu.js"></script>'
+var newJavaScript = '<script type="text/javascript" async="async" src="//raw.githubusercontent.com/w1770946466/Everyday_News/main/ad.js"></script>'
 let body = $response.body
     .replace(rHead, newStyle)
     .replace(rBody, newJavaScript);
