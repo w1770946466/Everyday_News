30d56950260dd28f04665cf00a1b7dbd0d4a173c
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-09-29 21:25:58 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,5 +1,5 @@
 let Oldone = '</style>'
-let Newone = 'a[href="https://www.87604411.com:9999/?channelCode=007"]{display:none!important}\ra[href="https://tz.kefuyuming.vip/3663.html"]{display:none!important}</style>';
+let Newone = 'a[href="https://www.87604411.com:9999/?channelCode=007"]{display:none!important}</style></style>a[href="https://tz.kefuyuming.vip/3663.html"]{display:none!important}</style>';
 let body = $response.body
 .replace(Oldone, Newone);
 $done({body});
