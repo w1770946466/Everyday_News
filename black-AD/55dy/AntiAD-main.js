d8b392bb19ef1212775f774d5bc74c6241a77c78
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-09-29 22:08:57 +0800

    Update AntiAD-main.js

--- black-AD/55dy/AntiAD-main.js
+++ black-AD/55dy/AntiAD-main.js
@@ -1,5 +1,5 @@
 let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/w1770946466/Everyday_News/main/black-AD/55dy/AntiAD-555dy.css" type="text/css" /><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/w1770946466/Everyday_News/main/black-AD/55dy/AntiAD-555dy.js"></script>'
 let body = $response.body
     .replace(ele, eleReplace)
 $done({ body });
