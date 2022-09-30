f19f49d898f3402070aa34153d7b488e4de6767e
北慕白 <63459931+w1770946466@users.noreply.github.com>
2022-09-30 09:36:53 +0800

    Update AntiAD-555dy.js

--- black-AD/55dy/AntiAD-555dy.js
+++ black-AD/55dy/AntiAD-555dy.js
@@ -1,5 +1,5 @@
 let Oldone = '</style>'
-let Newone = 'a[href="https://www.87604411.com:9999/?channelCode=007"]{display:none!important}';
+let Newone = '.is_mb{display:none!important}';
 let body = $response.body
 .replace(Oldone, Newone);
 $done({body});
