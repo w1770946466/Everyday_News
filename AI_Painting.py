3d7a010c3ffa883e20a834454f20a5185686d0de
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-01-13 20:00:11 +0800

    Update AI_Painting.py

--- AI_Painting.py
+++ AI_Painting.py
@@ -57,7 +57,7 @@ def qq_request(img_buffer):
     proxies = {
         "http":"http://27.42.168.46:55481",
     }
-    response = requests.post(url, json = obj, headers = headers, proxies=proxies, timeout = timeout)
+    response = requests.post(url, json = json.dumps(obj), headers = headers, proxies=proxies, timeout = timeout)
     data = response.json() or {}
     print(data)
     #return json.loads(data)
